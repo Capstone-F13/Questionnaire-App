3a37a414009e1487299486155f96d4560f6b7500
Andrew Kelley <andrew.kelley@codesummit.com>
2013-09-10 20:31:08 -0400

    Initial Commit

---
+++ Questionaire-App/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Questionaire-App
+//
+//  Created by Andrew Kelley on 9/10/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "Capstone_F13AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([Capstone_F13AppDelegate class]));
+    }
+}
