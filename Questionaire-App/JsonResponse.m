3e4de960666af5b9ee5172b442a053edd65bc80d
Adam Esterle <adam@adame.local>
2013-09-16 12:01:58 -0400

    Json
    Created basic json classes with some properties and methods

---
+++ Questionaire-App/JsonResponse.m
@@ -0,0 +1,14 @@
+//
+//  JsonResponse.m
+//  Questionaire-App
+//
+//  Created by Adam Esterle on 9/16/13.
+//
+//
+
+#import "JsonResponse.h"
+#import "Json.h"
+
+@implementation JsonResponse
+
+@end
