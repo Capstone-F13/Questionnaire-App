f523461cfb190dac355ac0770d566048e1c403ac
Tucker Cozzens <tcozzens@kent.edu>
2013-09-18 12:27:30 -0400

    Make view controllers for the "Main Menu" and "Record Song" pages of the app #5

---
+++ Questionaire_App/Questionaire_App/RecordSongViewController.m
@@ -0,0 +1,38 @@
+//
+//  RecordSongViewController.m
+//  Questionaire_App
+//
+//  Created by Tucker Cozzens on 9/18/13.
+//  Copyright (c) 2013 Tucker Cozzens. All rights reserved.
+//
+
+#import "RecordSongViewController.h"
+
+@interface RecordSongViewController ()
+
+@end
+
+@implementation RecordSongViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
