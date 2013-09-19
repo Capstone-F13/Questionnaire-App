4f48e339337a73e930047487e4ad53040435cddd
Mohammad Doleh <mdoleh@kent.edu>
2013-09-18 21:24:55 -0400

    Initial Commit
    Created view controller and associated xib file.

---
+++ Questionaire_App/Questionaire_App/SurveyViewController.h
@@ -0,0 +1,13 @@
+//
+//  SurveyViewController.h
+//  Questionaire_App
+//
+//  Created by Mohammad Doleh on 9/18/13.
+//  Copyright (c) 2013 Tucker Cozzens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SurveyViewController : UIViewController
+
+@end
---
+++ Questionaire_App/Questionaire_App/SurveyViewController.m
@@ -0,0 +1,38 @@
+//
+//  SurveyViewController.m
+//  Questionaire_App
+//
+//  Created by Mohammad Doleh on 9/18/13.
+//  Copyright (c) 2013 Tucker Cozzens. All rights reserved.
+//
+
+#import "SurveyViewController.h"
+
+@interface SurveyViewController ()
+
+@end
+
+@implementation SurveyViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
