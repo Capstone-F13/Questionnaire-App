c5c7b1c460c3d0a52f6c3ef1a2afd7109ca0b441
Adam Esterle <adam@adame.local>
2013-09-23 11:37:59 -0400

    #14 Survey

---
+++ Questionaire_App/Questionaire_App/Survey/Question.m
@@ -0,0 +1,14 @@
+//
+//  Question.m
+//  Questionaire_App
+//
+//  Created by Adam Esterle on 9/18/13.
+//  Copyright (c) 2013 Adam Esterle. All rights reserved.
+//
+
+#import "Question.h"
+#import "SurveyConstants.h"
+
+@implementation Question
+
+@end
