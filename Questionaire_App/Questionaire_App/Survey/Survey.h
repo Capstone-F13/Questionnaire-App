d65bbfa55dc62ceb468eaa79d7778ce5d079d0f9
Adam Esterle <adam@adame.local>
2013-09-22 23:58:21 -0400

    #14 First draft JSON parser
    Adding JSON classes (main one and response). Added a surbey class and a
    question class. Added a class to store constants.

---
+++ Questionaire_App/Questionaire_App/Survey/Survey.h
@@ -0,0 +1,17 @@
+//
+//  Survey.h
+//  Questionaire_App
+//
+//  Created by Adam Esterle on 9/18/13.
+//  Copyright (c) 2013 Adam Esterle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "SurveyConstants.h"
+#import "Question.h"
+
+@interface Survey : NSObject
+
+@property (nonatomic, strong) NSMutableArray *questions;
+
+@end
