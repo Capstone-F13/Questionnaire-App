d7d76d5577127928f29e6fa57182d59d55faaa0d
Tucker Cozzens <tcozzens@kent.edu>
2013-09-25 13:15:07 -0400

    Added implementations and organized views
    Implemented "Record Song"
    Made new page for "Song from Library"
    Implemented "Song from Library"

---
+++ Questionaire_App/Questionaire_App/SongLibraryViewController.h
@@ -0,0 +1,16 @@
+//
+//  SongLibraryViewController.h
+//  Questionaire_App
+//
+//  Created by Tucker Cozzens on 9/25/13.
+//  Copyright (c) 2013 Tucker Cozzens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MediaPlayer/MediaPlayer.h>
+#import <AVFoundation/AVFoundation.h>
+
+@interface SongLibraryViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+
+
+@end
