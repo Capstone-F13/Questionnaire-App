42d0e4c260fa8ceee5578b3473210b46c572dcbe
Tucker Cozzens <tcozzens@kent.edu>
2013-09-28 20:33:00 -0400

    fix?

--- Questionaire_App/Questionaire_App/RecordSongViewController.m
+++ Questionaire_App/Questionaire_App/RecordSongViewController.m
@@ -33,6 +33,66 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+<<<<<<< HEAD
 } 
+=======
+}
+
+- (IBAction)recordPauseTapped:(id)sender {
+    // Stop the audio player before recording
+    if (player.playing) {
+        [player stop];
+    }
+    
+    if (!recorder.recording) {
+        AVAudioSession *session = [AVAudioSession sharedInstance];
+        [session setActive:YES error:nil];
+        
+        // Start recording
+        [recorder record];
+        [_recordPauseButton setTitle:@"Pause" forState:UIControlStateNormal];
+        
+    } else {
+        
+        // Pause recording
+        [recorder pause];
+        [_recordPauseButton setTitle:@"Record" forState:UIControlStateNormal];
+    }
+    
+    [_stopButton setEnabled:YES];
+    [_playButton setEnabled:NO];
+}
+
+- (IBAction)stopTapped:(id)sender {
+    [recorder stop];
+     
+    AVAudioSession *audioSession = [AVAudioSession sharedInstance];
+    [audioSession setActive:NO error:nil];
+}
+
+- (void) audioRecorderDidFinishRecording:(AVAudioRecorder *)avrecorder successfully:(BOOL)flag{
+    [_recordPauseButton setTitle:@"Record" forState:UIControlStateNormal];
+    
+    [_stopButton setEnabled:NO];
+    [_playButton setEnabled:YES];
+}
+
+- (IBAction)playTapped:(id)sender {
+    if (!recorder.recording){
+        player = [[AVAudioPlayer alloc] initWithContentsOfURL:recorder.url error:nil];
+        [player setDelegate:self];
+        [player play];
+    }
+}
+
+- (void) audioPlayerDidFinishPlaying:(AVAudioPlayer *)player successfully:(BOOL)flag{
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle: @"Done"
+                                                    message: @"Finish playing the recording!"
+                                                   delegate: nil
+                                          cancelButtonTitle:@"OK"
+                                          otherButtonTitles:nil];
+    [alert show];
+}
+>>>>>>> Features----MM&RS
 
 @end
