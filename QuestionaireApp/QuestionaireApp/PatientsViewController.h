e85a51f1d98adb0ab80b8a459a5155126e41bff0
Mohammad Doleh <mdoleh@kent.edu>
2013-10-23 11:37:41 -0400

    Created Patients View Controller

---
+++ QuestionaireApp/QuestionaireApp/PatientsViewController.h
@@ -0,0 +1,13 @@
+//
+//  PatientsViewController.h
+//  QuestionaireApp
+//
+//  Created by Mohammad Doleh on 10/23/13.
+//  Copyright (c) 2013 Tucker Cozzens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PatientsViewController : UIViewController
+
+@end
