4bc772df41e54ece5275da1da544e6df969e67cb
Adam Esterle <adam@adame.local>
2013-10-27 22:21:54 -0400

    #37 Populate Patient List
    Patient list populates after login for the user to select which patient
    they want to login.

--- QuestionaireApp/QuestionaireApp/PatientsViewController.h
+++ QuestionaireApp/QuestionaireApp/PatientsViewController.h
@@ -8,6 +8,8 @@
 
 #import <UIKit/UIKit.h>
 
-@interface PatientsViewController : UIViewController
+@interface PatientsViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
+
+@property (nonatomic, strong) IBOutlet UITableView *patientTable;
 
 @end
--- QuestionaireApp/QuestionaireApp/PatientsViewController.m
+++ QuestionaireApp/QuestionaireApp/PatientsViewController.m
@@ -7,12 +7,18 @@
 //
 
 #import "PatientsViewController.h"
+#import "AFHTTPRequestOperationManager.h"
 
 @interface PatientsViewController ()
+
+@property (strong) AFHTTPRequestOperationManager *manager;
+@property (nonatomic, retain) NSArray *patients;
 
 @end
 
 @implementation PatientsViewController
+
+@synthesize patientTable;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -23,10 +29,80 @@
     return self;
 }
 
+- (id)initWithCoder:(NSCoder *)aDecoder {
+    if (self = [super initWithCoder:aDecoder]) {
+        self.manager = [AFHTTPRequestOperationManager manager];
+        
+        NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+        self.patients = [defaults arrayForKey:@"patients"];
+        
+        NSLog(@"%@ %@", [self.patients objectAtIndex:0], [self.patients objectAtIndex:1]);
+        
+        //[[self patientTable] initWithFrame:CGRectMake(100, 100, 200, 200) style:UITableViewStyleGrouped];
+        //[[self patientTable] reloadData];
+        
+    }
+    return self;
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return [self.patients count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"patientCell"];
+    NSLog(@"Here");
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"patientCell"];
+    }
+    
+    cell.textLabel.text = [self.patients objectAtIndex:indexPath.row];
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    
+    NSString *URLString = @"http://capstone-f13.herokuapp.com/oauth2/access_token";
+    NSString *token = [defaults stringForKey:@"autoToken"];
+    NSString *patientId = [self.patients objectAtIndex:indexPath.row];
+    
+    NSDictionary *parameters = @{@"token" : token,
+                                 @"patient_id" : patientId
+                                 };
+    
+    AFHTTPRequestOperation *operation = [self.manager POST:URLString
+                                                parameters:parameters
+        success:^(AFHTTPRequestOperation *operation, id responseObject) {
+                                                       
+            NSLog(@"%@", responseObject);
+            [defaults setObject:patientId forKey:@"patientID"];
+            [defaults synchronize];
+
+        } failure:^(AFHTTPRequestOperation *operation, NSError *localError) {
+           
+           NSLog(@"Error: %@", localError);
+           
+        }];
+    
+    [operation start];
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    
+    [[self patientTable] setDataSource:self];
+    [[self patientTable] setDelegate:self];
+    
 }
 
 - (void)didReceiveMemoryWarning
